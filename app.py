def84662ab2b227ec54a0831fc5722470a97dba6
romqrq <rjaalbuquerque@gmail.com>
2019-12-18 18:20:34 +0000

    all working

--- app.py
+++ app.py
@@ -29,6 +29,10 @@ def get_tasks():
     #directly to MONGO. The FIND() method will return everything from our tasks collection
     return render_template('tasks.html', tasks=mongo.db.tasks.find())
 
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
