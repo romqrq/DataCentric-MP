3e4ca68cf2824f4ad0a1a9536134ec7e3a65c59c
romqrq <rjaalbuquerque@gmail.com>
2019-12-20 16:21:10 +0000

    Added edit and delete functionalities

--- app.py
+++ app.py
@@ -73,10 +73,30 @@ def update_task(task_id):
     })
     return redirect(url_for('get_tasks'))
 
-@app_route('/delete_task/<task_id>')
+@app.route('/delete_task/<task_id>')
 def delete_task(task_id):
     mongo.db.tasks.delete_one({'_id': ObjectId(task_id)})
-    return redirect(urf_for('get_tasks'))
+    return redirect(url_for('get_tasks'))
+
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html',
+    categories = mongo.db.categories.find())
+
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+    category = mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update({'_id': ObjectId(category_id)}, {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))
+
+@app.route('/delete_category/<category_id>')
+def delete_category(category.id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
